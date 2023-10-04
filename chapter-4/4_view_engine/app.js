7c424083c031e56c81c89412bf38e691cbece33e
Tatang Romadhona <romadhonatatang@gmail.com>
2023-10-04 21:11:20 +0700

    feat: add view engine example

--- chapter-4/4_view_engine/app.js
+++ chapter-4/4_view_engine/app.js
@@ -2,7 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { PORT = 3000 } = process.env;
+const pool = require('./externals/postgres');
 
 app.use(express.urlencoded());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('welcome', { appName: 'Binar App' });
+});
+
+app.get('/posts', async (req, res) => {
+    let result = await pool.query('SELECT * FROM posts ORDER BY id');
+    res.render('getAllPost', { posts: result.rows });
+});
+
+app.get('/posts/create', async (req, res) => {
+    res.render('createPost');
+});
+
+app.post('/posts', async (req, res) => {
+    const { title, body } = req.body;
+    await pool.query("INSERT INTO posts (title, body) values ($1, $2) RETURNING *;", [title, body]);
+
+    res.redirect('/posts');
+});
 
 app.listen(PORT, () => console.log('listening on port', PORT));
---
+++ chapter-4/4_view_engine/externals/postgres.js
@@ -0,0 +1,18 @@
+const Pool = require('pg').Pool;
+const {
+    DB_USER,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_PORT = 5432,
+    DB_HOST
+} = process.env;
+
+const pool = new Pool({
+    user: DB_USER,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    host: DB_HOST
+});
+
+module.exports = pool;
